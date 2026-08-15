a4efe02b889be35149d6313bece25071677cf076
jinwooLee <ejo01110111@gmail.com>
2026-08-15 07:40:27 +0000

    init redis-4356

---
+++ redis/redis-4356/fragtrigger.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo docker exec redis-4356-test redis-benchmark -c 20 -r 500000 -n 10000000 -t set -d 3500 -q &
+sudo docker exec redis-4356-test redis-benchmark -c 20 -r 500000 -n 10000000 -t set -d 300 -q &
+sudo docker exec redis-4356-test redis-benchmark -c 20 -r 500000 -n 10000000 -t set -d 1500 -q &
