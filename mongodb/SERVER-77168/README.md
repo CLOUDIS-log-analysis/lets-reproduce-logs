d76b5f95f78464f3cb52efa987bedfd7825a17ac
gjip <rjsdn247@gmail.com>
2026-08-22 17:40:34 +0900

    mongodb: SERVER-77168: add log relevance

--- mongodb/SERVER-77168/README.md
+++ mongodb/SERVER-77168/README.md
@@ -1,4 +1,4 @@
-# SERVER-77168
+# [SERVER-77168](https://jira.mongodb.org/browse/SERVER-77168)
 Time Series Collection을 mongorestore로 복원할 때 mongodb가 crash하는 버그이다.
 
 # 재현 방법
@@ -14,6 +14,19 @@ sudo chown $(id -u) log
 
 ```
 
+# 원인 및 수정
 
+유저의 잘못된 입력을 받고 서버가 강제종료 되도록 한것이 원인이다.
 
+수정된 버전에서는 잘못된 입력에 대해 예외처리로 대응하여 강제종료가 일어나지 않도록 한다.
 
+# 로그 연관성
+
+[수정 커밋](https://github.com/mongodb/mongo/commit/75b3b24942dec1877602d294b91aa46f87abc4e2)
+
+핵심 로그:
+```json
+{"t":{"$date":"2026-08-15T07:38:50.073+00:00"},"s":"F",  "c":"ASSERT",   "id":23079,   "ctx":"conn26","msg":"Invariant failure","attr":{"expr":"!collectionName.startsWith(\"system.buckets.\")","file":"src/mongo/db/auth/resource_pattern.h","line":133}}
+```
+
+로그가 가리키는 코드 위치와 실제로 수정된 코드가 일치하여 로그 연관성이 매우 높다.
--- mongodb/SERVER-77168/repro.sh
+++ mongodb/SERVER-77168/repro.sh
@@ -5,13 +5,21 @@ mongod --port 3000 --dbpath data --logpath /data/mongod.log --wiredTigerCacheSiz
 mongosh --port 3000 --eval 'rs.initiate()'
 mongosh admin --port 3000 --eval "db.createUser({user: 'user', pwd: 'password', roles: ['root']})"
 
+sleep 1
+
 # create & insert data into a time series collection
 mongosh --port 3000 --username "user" --password "password" --eval 'db.setLogLevel(5)'
 mongosh --port 3000 --username "user" --password "password" --eval 'db.createCollection( "weather", { timeseries: { timeField: "timestamp", metaField: "metadata", granularity: "hours" } } )'
 mongosh --port 3000 --username "user" --password "password" --eval 'db.weather.insertOne( { "metadata": { "sensorId": 5578, "type": "temperature" }, "timestamp": ISODate("2021-05-18T00:00:00.000Z"), "temp": 12 } )'
 
+sleep 1
+
 # dump
 mongodump --port 3000 --username "user" --password "password"
 
+sleep 1
+
 # restore
 mongorestore --port 3000 --drop --username "user" --password "password" dump/test/system.buckets.weather.bson
+
+sleep 10
